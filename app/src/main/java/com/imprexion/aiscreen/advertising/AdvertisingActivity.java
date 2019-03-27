4c13201d82f5a9fcf6957eab1fb7d363755dd527
longbin <460566742@qq.com>
2019-03-27 15:11:15 +0800

    fix：修改根据contentPlay的计划播放广告。

--- app/src/main/java/com/imprexion/aiscreen/advertising/AdvertisingActivity.java
+++ app/src/main/java/com/imprexion/aiscreen/advertising/AdvertisingActivity.java
@@ -50,18 +50,23 @@ public class AdvertisingActivity extends AppCompatActivity implements View.OnCli
     private AnimatorSet mAnimatorSet_2 = new AnimatorSet();
     private int i;
     private ContentPlay mContentPlay;
+    private List<ContentPlay> mContentPlayList;
     private boolean isTimeToShowAd;
     private long startTime;
+    private String[] picUrl = {"https://cn.bing.com/th?id=OHR.SakuraFes_ZH-CN1341601988_1920x1080.jpg&rf=NorthMale_1920x1080.jpg"};
+    private String[] picUrl1 = {"https://cn.bing.com/sa/simg/hpb/NorthMale_EN-US8782628354_1366x768.jpg"};
+    private String[] picUrl2 = {"https://cn.bing.com/th?id=OHR.AthensNight_ZH-CN1280970241_1920x1080.jpg&rf=NorthMale_1920x1080.jpg"};
+    private List<String[]> mList = new ArrayList<>();
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            String[] picUrl = mContentPlay.getContentInfo().getContentUrl();
+            String[] picUrl = ((ContentPlay) msg.obj).getContentInfo().getContentUrl();
             super.handleMessage(msg);
             switch (msg.what) {
                 case NEXT_AD:
                     Glide.with(AdvertisingActivity.this)
 //                            .load(mAdpath + "/" + mImges.get(i++ % mImges.size()))
-                            .load(picUrl[i++ % picUrl.length])
+                            .load(picUrl[0])
                             .into(ivAd);
                     mAnimatorSet.play(mAdEnterObjAnimator).with(mAdExitObjAnimator_1);
                     mAnimatorSet.setDuration(300);
@@ -71,7 +76,7 @@ public class AdvertisingActivity extends AppCompatActivity implements View.OnCli
                 case NEXT_AD_2:
                     Glide.with(AdvertisingActivity.this)
 //                            .load(mAdpath + "/" + mImges.get(i++ % mImges.size()))
-                            .load(picUrl[i++ % picUrl.length])
+                            .load(picUrl[0])
                             .into(ivAd2);
                     mAnimatorSet_2.play(mAdEnterObjAnimator_1).with(mAdExitObjAnimator);
                     mAnimatorSet_2.setDuration(300);
@@ -94,37 +99,34 @@ public class AdvertisingActivity extends AppCompatActivity implements View.OnCli
         mAdEnterObjAnimator_1 = ObjectAnimator.ofFloat(ivAd2, "translationX", 1080, 0);
         mAdExitObjAnimator_1 = ObjectAnimator.ofFloat(ivAd2, "translationX", 0, -1080);
 //        getSupportFragmentManager().beginTransaction().add(R.id.fl_fragment, new GestureActivationFragment()).commitAllowingStateLoss();
-
         rlAdvertising.setOnClickListener(this);
         //test
         initContentPlay();
-
-//        downloadImges();
         getImges();
 
     }
 
-    private void downloadImges() {
-
-    }
-
     private void initContentPlay() {
-        if (mContentPlay == null) {
+        mContentPlayList = new ArrayList<>();
+        mList.add(picUrl);
+        mList.add(picUrl1);
+        mList.add(picUrl2);
+        for (int j = 0; j < 2000; j++) {
             mContentPlay = new ContentPlay();
             ContentPlay.ContentInfo contentInfo = new ContentPlay.ContentInfo();
             contentInfo.setContent_len(5000);
             contentInfo.setContentName("播放广告");
             contentInfo.setContent_priority(5);
-            String[] picUrl = {"https://cn.bing.com/sa/simg/hpb/NorthMale_EN-US8782628354_1920x1080.jpg"};
-            contentInfo.setContentUrl(picUrl);
+            contentInfo.setContentUrl(mList.get(j % 3));
             mContentPlay.setContentInfo(contentInfo);
-            mContentPlay.setStart_time(1553572838);
+            mContentPlay.setStart_time(1553667779 + j * 10);
+            mContentPlayList.add(mContentPlay);
         }
     }
 
     private void getImges() {
         mImges = Tools.getFilesAllName(mAdpath);
-        Log.d(TAG, "mImges size = " + mImges.size());
+//        Log.d(TAG, "mImges size = " + mImges.size());
 
     }
 
@@ -137,19 +139,34 @@ public class AdvertisingActivity extends AppCompatActivity implements View.OnCli
                 @Override
                 public void run() {
                     int i = 0;
-                    startTime = mContentPlay.getStart_time();
+                    int k = 0;
+                    int size = mContentPlayList.size();
                     do {
-                        isTimeToShowAd = System.currentTimeMillis() == startTime ? true : false;
+                        mContentPlay = mContentPlayList.get(k++ % size);
+                        startTime = mContentPlay.getStart_time();
+                    } while (System.currentTimeMillis() / 1000 > startTime);
+                    do {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "System.currentTimeMillis()=" + System.currentTimeMillis() / 1000);
+                        isTimeToShowAd = System.currentTimeMillis() / 1000 == startTime ? true : false;
                         if (isTimeToShowAd) {
                             mMessage = mHandler.obtainMessage();
                             i = i == 1 ? 0 : 1;
                             mMessage.what = i + 1;
+                            mMessage.obj = mContentPlay;
                             mHandler.sendMessage(mMessage);
-                            try {
-                                Thread.sleep(15000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
+//                            try {
+//                                Thread.sleep(mContentPlay.getContentInfo().getContent_len());
+//                            } catch (InterruptedException e) {
+//                                e.printStackTrace();
+//                            }
+                            mContentPlay = mContentPlayList.get(k++ % size);
+                            startTime = mContentPlay.getStart_time();
+                            isTimeToShowAd = false;
                         }
                     } while (true);
                 }
