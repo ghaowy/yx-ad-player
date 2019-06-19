3bebb0fba52258e75af976e2b6a1d18670e291b5
bin.long <long.bin@imprexion.cn>
2019-06-19 17:18:07 +0800

    fix: 删除bugly日志上传

--- app/src/main/java/com/imprexion/adplayer/base/ADPlayApplication.java
+++ app/src/main/java/com/imprexion/adplayer/base/ADPlayApplication.java
@@ -3,7 +3,6 @@ package com.imprexion.adplayer.base;
 import com.imprexion.adplayer.tools.Tools;
 import com.imprexion.library.YxLog;
 import com.imprexion.library.base.BaseApplication;
-import com.tencent.bugly.crashreport.CrashReport;
 
 public class ADPlayApplication extends BaseApplication {
 
@@ -14,7 +13,6 @@ public class ADPlayApplication extends BaseApplication {
     public void onCreate() {
         super.onCreate();
         adPlayApplication = this;
-        CrashReport.initCrashReport(getApplicationContext(), "8e26a11a85", false);
         YxLog.i(TAG, "Application onCreate: version name is " + Tools.getVersionName(this));
     }
 
