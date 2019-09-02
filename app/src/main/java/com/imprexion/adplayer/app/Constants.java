f40fe56638fae2c940018c57e11724ef590f1dc9
ghaowy <ghaowy@163.com>
2019-09-02 16:37:03 +0800

    feat: 增加启动应用埋点

---
+++ app/src/main/java/com/imprexion/adplayer/app/Constants.java
@@ -0,0 +1,11 @@
+package com.imprexion.adplayer.app;
+
+/**
+ * @author : yan
+ * @date : 2019/9/2 16:35
+ * @desc : TODO
+ */
+public class Constants {
+
+    public static final String KEY_FROM = "key_from";
+}
