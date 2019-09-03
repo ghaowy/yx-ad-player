d277670be5157045a85f981d8d3d3d1228664616
ghaowy <ghaowy@163.com>
2019-09-03 10:29:18 +0800

    fix: 修改轮播应用Http配置为 全局配置

--- app/src/main/java/com/imprexion/adplayer/net/http/HttpADManager.java
+++ app/src/main/java/com/imprexion/adplayer/net/http/HttpADManager.java
@@ -1,15 +1,11 @@
 package com.imprexion.adplayer.net.http;
 
 import android.os.Build;
-import android.text.TextUtils;
 
 import com.imprexion.adplayer.bean.ADContentPlay;
-import com.imprexion.adplayer.net.config.HttpParamsConfig;
 import com.imprexion.adplayer.player.PlayerModel;
-import com.imprexion.library.YxConfig;
 import com.imprexion.library.YxHttp;
 import com.imprexion.library.YxLog;
-import com.imprexion.library.net.http.HttpConfig;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -25,12 +21,10 @@ import io.reactivex.schedulers.Schedulers;
 public class HttpADManager {
     private static final String TAG = "HttpADManager";
     private static HttpADManager mHttpManager;
-    private HttpParamsConfig mHttpConfig;
     private PlayerModel.onPlayerDataListener mAdListener;
     private Disposable mDisposable;
 
     private HttpADManager() {
-        initHttp();
     }
 
     public static HttpADManager getInstance() {
@@ -44,33 +38,14 @@ public class HttpADManager {
         return mHttpManager;
     }
 
-    private void initHttp() {
-        mHttpConfig = YxConfig.get(HttpParamsConfig.class);
-        if (mHttpConfig == null) {
-            YxLog.e(TAG, "HttpParamsConfig CONFIG IS NULL");
-            return;
-        }
-        YxLog.d(TAG, "baseUrl= " + mHttpConfig.getBaseUrl() + " token= " + mHttpConfig.getToken());
-        HttpConfig config = new HttpConfig.Builder()
-                .baseUrl(mHttpConfig.getBaseUrl())
-                .addHeader("token", mHttpConfig.getToken())
-                .build();
-        YxHttp.setDefaultConfig(config);
-    }
-
     public void setonPlayerDataListener(PlayerModel.onPlayerDataListener listener) {
         mAdListener = listener;
     }
 
     public void getAdData(String playDate) {
-        if (mHttpConfig == null || TextUtils.isEmpty(mHttpConfig.getToken())) {
-            YxLog.e("sorry, token or config is NULL");
-            return;
-        }
-
         mDisposable = YxHttp.getDefaultInstance()
                 .service(IAdRequest.class)
-                .getAdDatas(Build.SERIAL, playDate, mHttpConfig.getToken())
+                .getAdDatas(Build.SERIAL, playDate)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<BaseResult<ADContentPlay>>() {
