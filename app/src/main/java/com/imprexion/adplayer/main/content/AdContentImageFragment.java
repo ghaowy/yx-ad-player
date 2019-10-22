47ca7ed945806d0613bce7c8046eca2f95a7c147
ghaowy <ghaowy@163.com>
2019-10-22 15:02:08 +0800

    fix: 修改视频播放策略

--- app/src/main/java/com/imprexion/adplayer/main/content/AdContentImageFragment.java
+++ app/src/main/java/com/imprexion/adplayer/main/content/AdContentImageFragment.java
@@ -8,10 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.imprexion.adplayer.R;
 import com.imprexion.adplayer.tools.Tools;
-import com.imprexion.adplayer.video.IjkPlayView;
+import com.imprexion.adplayer.video.VideoPlayerPresenter;
 import com.imprexion.library.YxLog;
 
 import org.jetbrains.annotations.NotNull;
@@ -34,12 +35,17 @@ public class AdContentImageFragment extends Fragment implements IMediaPlayer.OnP
     private String mUrl;
     private boolean mIsVideo;
     private boolean isInit;
-    private IjkPlayView mIjkPlayView;
+    private RelativeLayout mRlContainer;
+    private VideoPlayerPresenter mVideoPlayerPresenter;
 
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
+        isCanLoadData();
+    }
+
+    private void isCanLoadData() {
         if (!isInit) {
             return;
         }
@@ -58,6 +64,11 @@ public class AdContentImageFragment extends Fragment implements IMediaPlayer.OnP
         if (mIsVideo) {
             YxLog.i(TAG, "onVisible " + mIsVideo);
             loadVideo();
+        } else {
+            YxLog.i(TAG, " playPic --> url= " + mUrl);
+            Tools.showPicWithGlide(ivAdFragment, mUrl);
+            mRlContainer.setVisibility(View.GONE);
+            ivAdFragment.setVisibility(View.VISIBLE);
         }
     }
 
@@ -76,21 +87,22 @@ public class AdContentImageFragment extends Fragment implements IMediaPlayer.OnP
         View view = inflater.inflate(R.layout.fragment_ad_content, container, false);
         initView(view);
         isInit = true;
+        isCanLoadData();
         return view;
     }
 
     private void initView(View view) {
         ivAdFragment = view.findViewById(R.id.iv_ad_fragment);
-        mIjkPlayView = view.findViewById(R.id.ijk_player_view);
+        mRlContainer = view.findViewById(R.id.rl_container);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (mIjkPlayView == null) {
+        if (mVideoPlayerPresenter == null) {
             return;
         }
-        mIjkPlayView.stopPlayback();
+        mVideoPlayerPresenter.release();
     }
 
     @Override
@@ -106,34 +118,25 @@ public class AdContentImageFragment extends Fragment implements IMediaPlayer.OnP
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        if (!mIsVideo) {
-            YxLog.i(TAG, " playPic --> url= " + mUrl);
-            Tools.showPicWithGlide(ivAdFragment, mUrl);
-            mIjkPlayView.setVisibility(View.GONE);
-            ivAdFragment.setVisibility(View.VISIBLE);
-        }
-    }
-
-    @Override
     public void onDetach() {
         super.onDetach();
         YxLog.i(TAG, "onDetach");
-        if (mIjkPlayView != null) {
-            mIjkPlayView.release(true);
+        if (mVideoPlayerPresenter != null) {
+            mVideoPlayerPresenter.release();
         }
     }
 
     public void loadVideo() {
         YxLog.i(TAG, "loadVideo mUrl= " + mUrl);
         if (mIsVideo) {
-            mIjkPlayView.setVisibility(View.VISIBLE);
+//            mIjkPlayView.setVisibility(View.VISIBLE);
             ivAdFragment.setVisibility(View.GONE);
-            mIjkPlayView.setVideoPath(getProxy(getContext()).getProxyUrl(mUrl));
-            mIjkPlayView.setOnPreparedListener(this);
-            mIjkPlayView.setOnCompletionListener(this);
-            mIjkPlayView.setOnErrorListener(this);
+            mVideoPlayerPresenter = new VideoPlayerPresenter(mRlContainer);
+            mVideoPlayerPresenter.setVideoPath(getProxy(getContext()).getProxyUrl(mUrl));
+//            mIjkPlayView.setVideoPath(getProxy(getContext()).getProxyUrl(mUrl));
+//            mIjkPlayView.setOnPreparedListener(this);
+//            mIjkPlayView.setOnCompletionListener(this);
+//            mIjkPlayView.setOnErrorListener(this);
         }
     }
 
@@ -147,7 +150,6 @@ public class AdContentImageFragment extends Fragment implements IMediaPlayer.OnP
 
     @Override
     public void onCompletion(IMediaPlayer iMediaPlayer) {
-
     }
 
     @Override
